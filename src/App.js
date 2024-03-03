c28b2c9379e363a638619c9dfc16d9807fd24355
Shubham Kumar <shubhammuz110203@gmail.com>
2024-03-03 21:36:34 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
